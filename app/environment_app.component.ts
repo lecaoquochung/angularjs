625a7540a6bb31290ac63e68990cab9dd9d33db3
HL <lecaoquochung@gmail.com>
2016-11-13 08:25:05 +0900

    [logic] First Angularjs 2 Component

---
+++ app/environment_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app'
+})
+
+@View({
+  template: '<h2>Project learning Angularjs 2</h2>'
+})
+
+export class AppComponent {
+
+}
